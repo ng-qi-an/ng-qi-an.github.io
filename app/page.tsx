fb961207f644a81427dd76b78f187da9005dc962
Ng Qi An <ng.qi.an@sji.edu.sg>
2024-07-28 15:10:28 +0800

    Minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
-import Icon from "./components/Icon";
+import Icon from "../components/Icon";
 
 export default function Page(){
-  return <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+  return <>
+  <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center px-[50px]">
     {/* Radial gradient for the container to give a faded look */}
     <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-    <div className="w-full flex flex-col items-center justify-center">
-      <div className="w-full flex items-center justify-center gap-2">
-        <h1 className="cabin flex items-center text-2xl sm:text-5xl font-semibold relative z-20 bg-clip-text text-transparent bg-gradient-to-b  from-neutral-500 dark:from-neutral-200 to-neutral-500 py-2">
-          Hey there 👋🏻
+  </div>
+  <div className="w-screen flex items-center px-[70px] justify-center h-screen absolute z-[10] top-0 left-0">
+    <img src="/pfp.jpg" className="min-w-[200px] h-[200px] object-cover object-left rounded-xl mr-8"/>
+    <div className="w-full flex flex-col gap-2">
+      <div className="flex items-center">
+        <h1 className="cabin flex items-center text-2xl sm:text-3xl font-semibold relative z-20 text-neutral-500 dark:text-neutral-500 py-2">
+          Hey there 
         </h1>
+        <p className="text-4xl ml-2">👋</p>
       </div>
-      <h1 className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-blue-500 dark:from-blue-300 to-blue-500 py-4">
+      <h1 className="text-4xl sm:text-6xl font-bold relative z-20 text-black dark:text-white py-2">
         I'm Qi An
       </h1>
     </div>
-</div>
+  </div>
+  </>
 }
